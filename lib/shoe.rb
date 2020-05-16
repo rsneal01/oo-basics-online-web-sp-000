3b7a7f1199cd3ead8b609f362cac729e6dc58c54
Bobby Neal <rsneal01@gmail.com>
2020-05-16 17:05:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,12 @@
 # Make your shoe class here!
 
 class Shoe 
+  attr_reader :brand, :color, :size, :material, :condition
+  
   def initialize(brand)
     @brand = brand
   end
   
-  def brand
-    @brand
-  end
+  
+  
 end
